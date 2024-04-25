3968c2934be01ef644f6c355f34ba06adff261ae
SethA13 <seth1309@gmail.com>
2024-04-25 08:14:32 -0600

    Flake for nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Word Cipher";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # insert desired packages here
+        gcc
+        cmake
+      ];
+    };
+  };
+}
