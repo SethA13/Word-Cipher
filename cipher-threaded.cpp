99ca6430b2aeaa5e9a044c3a3a75f80511d10927
SethA13 <seth1309@gmail.com>
2024-04-30 07:41:35 -0600

    Style update

--- cipher-threaded.cpp
+++ cipher-threaded.cpp
@@ -9,8 +9,6 @@
 #include <mutex>
 #include <map>
 
-using namespace std;
-
 std::unordered_set<std::string> generateAllPermutations(const std::string& str) 
 {
   std::unordered_set<std::string> permutations;
@@ -39,33 +37,33 @@ std::unordered_set<std::string> generateAllPermutations(const std::string& str)
   return permutations;
 }
 
-string getLetters()
+std::string getLetters()
 {
-  string letters;
+  std::string letters;
   // Prompt for letters
-  cout << "Enter letters: ";
-  cin >> letters;
+  std::cout << "Enter letters: ";
+  std::cin >> letters;
   return letters;
 }
 
-string getDictionaryFile()
+std::string getDictionaryFile()
 {
-  string dictionaryFile;
-  cin >> dictionaryFile;
+  std::string dictionaryFile;
+  std::cin >> dictionaryFile;
   return dictionaryFile;
 }
 
-unordered_set<string> readDictionary(string location) // No need to pass dictionary as argument
+std::unordered_set<std::string> readDictionary(std::string location) // No need to pass dictionary as argument
 {
-  unordered_set<string> dictionary; // Create a new dictionary here
-  ifstream dictionaryFile(location);
+  std::unordered_set<std::string> dictionary; // Create a new dictionary here
+  std::ifstream dictionaryFile(location);
   if (!dictionaryFile)
   {
-    cout << "dictionary not found, please enter a filename. ";
+    std::cout << "dictionary not found, please enter a filename. ";
     location = getDictionaryFile();
     return readDictionary(location); // Return here
   }
-  string word;
+  std::string word;
   while (dictionaryFile >> word)
   {
     dictionary.insert(word);
@@ -74,143 +72,68 @@ unordered_set<string> readDictionary(string location) // No need to pass diction
   return dictionary;
 }
 
-void compareWords(const unordered_set<string>& permutations, const unordered_set<string>& dictionary, mutex& mtx, map<int, vector<string>>& categorizedWords)
+void compareWords(const std::unordered_set<std::string>& permutations, const std::unordered_set<std::string>& dictionary, std::mutex& mtx, std::map<int, std::set<std::string>>& categorizedWords)
 {
   for (auto permutation : permutations)
   {
     if (dictionary.find(permutation) != dictionary.end())
     {
-      lock_guard<mutex> lock(mtx); // Lock the mutex to ensure thread-safe printing
+      std::lock_guard<std::mutex> lock(mtx); // Lock the mutex to ensure thread-safe printing
 
       // Categorize the word by its length
-      categorizedWords[permutation.length()].push_back(permutation);
+      categorizedWords[permutation.length()].insert(permutation);
     }
   }
 }
 
 int main()
 {
-  unordered_set<string> dictionary;
+  std::unordered_set<std::string> dictionary;
   dictionary = readDictionary("words_alpha.txt");
-  string letters = getLetters();
-  unordered_set<string> permutations = generateAllPermutations(letters);
-  cout << "Number of permutations: " << permutations.size() << endl;
+  std::string letters = getLetters();
+  std::unordered_set<std::string> permutations = generateAllPermutations(letters);
+  std::cout << "Number of permutations: " << permutations.size() << std::endl;
 
-  mutex mtx; // Mutex for thread synchronization
-  map<int, vector<string>> categorizedWords; // Categorized words by length
+  std::mutex mtx; // Mutex for thread synchronization
+  std::map<int, std::set<std::string>> categorizedWords; // Categorized words by length
 
   // Split permutations into chunks for parallel processing
-  const size_t numThreads = thread::hardware_concurrency();
-  vector
-  <thread> threads(numThreads);
-  vector
-  <unordered_set<string>> chunkedPermutations(numThreads);
+  const size_t numThreads = std::thread::hardware_concurrency();
+  std::vector<std::thread> threads(numThreads);
+  std::vector<std::unordered_set<std::string>> chunkedPermutations(numThreads);
 
   size_t chunkSize = permutations.size() / numThreads;
   auto it = permutations.begin();
   for (size_t i = 0; i < numThreads; ++i) {
     auto start = it;
     std::advance(it, chunkSize);
-    chunkedPermutations[i] = unordered_set<string>(start, it);
+    chunkedPermutations[i] = std::unordered_set<std::string>(start, it);
   }
 
   // Launch threads to compare permutations against the dictionary
   for (size_t i = 0; i < numThreads; ++i) {
-    threads[i] = thread(compareWords, ref(chunkedPermutations[i]), ref(dictionary), ref(mtx), ref(categorizedWords));
+    threads[i] = std::thread(compareWords, ref(chunkedPermutations[i]), ref(dictionary), ref(mtx), ref(categorizedWords));
   }
 
   // Join threads
   for (size_t i = 0; i < numThreads; ++i) {
     threads[i].join();
   }
-  set<string> oneLetterWords;
-  set<string> twoLetterWords;
-  set<string> threeLetterWords;
-  set<string> fourLetterWords;
-  set<string> fiveLetterWords;
-  set<string> sixLetterWords;
-  set<string> allOthers;
 
+  int totalWordsFound = 0;
   for (auto entry : categorizedWords)
   {
+    int wordsFound = 0;
+    std::cout << entry.first << " Letter words;" << std::endl;
     for (const auto& word : entry.second)
     {
-      switch (entry.first)
-      {
-      case 1:
-        oneLetterWords.insert(word);
-        break;
-      case 2:
-        twoLetterWords.insert(word);
-        break;
-      case 3:
-        threeLetterWords.insert(word);
-        break;
-      case 4:
-        fourLetterWords.insert(word);
-        break;
-      case 5:
-        fiveLetterWords.insert(word);
-        break;
-      case 6:
-        sixLetterWords.insert(word);
-        break;
-      
-      default:
-        allOthers.insert(word);
-        break;
-      }
+      std::cout << word << std::endl;
+      wordsFound++;
+      totalWordsFound++;
     }
+    std::cout << entry.first << " Letter words found; " << wordsFound << std::endl;
+    std::cout << std::endl;
   }
-  
-  cout << "One letter words: " << endl;
-  for (auto word : oneLetterWords)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
-  cout << "Two letter words: " << endl;
-  for (auto word : twoLetterWords)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
-  cout << "Three letter words: " << endl;
-  for (auto word : threeLetterWords)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
-  cout << "Four letter words: " << endl;
-  for (auto word : fourLetterWords)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
-  cout << "Five letter words: " << endl;
-  for (auto word : fiveLetterWords)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
-  cout << "Six letter words: " << endl;
-  for (auto word : sixLetterWords)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
-  cout << "All others: " << endl;
-  for (auto word : allOthers)
-  {
-    cout << word << endl;
-  }
-  cout << endl;
-
+  std::cout << "Total words found; " << totalWordsFound << std::endl;
   return 0;
 }
